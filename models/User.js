389bfbf91ca1d5f2963c9194d2eccd4f7e5ca2e3
Emma <emmahodson23@gmail.com>
2018-10-26 16:11:14 +0100

    first commit

---
+++ models/User.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const UserSchema = new Schema({
+  username: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  // password: {
+  //   type: String,
+  //   required: true,
+  // },
+  profilePicture: {
+    type: String,
+  },
+  address: {
+    houseNumber: String,
+    street: String,
+    city: String,
+    postcode: String,
+    telephone: String,
+  }
+});
+
+module.exports = mongoose.model('users', UserSchema);
