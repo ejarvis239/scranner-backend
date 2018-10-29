8a99b30df8f8ad352194463beb13af813f7571ea
Rachael Hall <rachael.hall.889@gmail.com>
2018-10-29 16:31:48 +0000

    change getRecipes response

--- controllers/recipes.js
+++ controllers/recipes.js
@@ -4,7 +4,7 @@ const getRecipesByUser = (req, res, next) => {
   Recipe.find({ user: req.params.user_id })
     .populate('user')
     .then((recipes) => {
-      res.status(200).send(recipes);
+      res.status(200).send({ recipes });
     })
     .catch(next);
 };
