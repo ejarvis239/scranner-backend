a635d37536f05077f577baea83136fec24e4e37a
Rachael Hall <rachael.hall.889@gmail.com>
2018-10-29 17:11:36 +0000

    testing for POST new user

--- spec/index.spec.js
+++ spec/index.spec.js
@@ -1,44 +1,68 @@
 process.env.NODE_ENV = 'development';
-const data = require('../seed/testData/index.js')
-const seedDB = require('../seed/seed.js');
-const mongoose = require('mongoose');
 const { expect } = require('chai');
+const mongoose = require('mongoose');
 const app = require('../app.js');
 const request = require('supertest')(app);
+const seedDB = require('../seed/seed.js');
+const data = require('../seed/testData/index.js');
 
-describe('/api', () => {
+describe('/api', function () {
+  this.timeout(6000);
   let users, recipes, shoppingData;
+
   beforeEach(function () {
     return seedDB(data)
       .then((docs) => {
         [users, recipes, shoppingData] = docs
       })
   });
-  after(() => mongoose.disconnect())
 
-  describe("/*", () => {
-    it('GET any other path returns status 404 and message Page not found', () => {
+  after(() => mongoose.disconnect());
+
+  describe('/*', () => {
+    it('GET invalid path returns status 404 and message Page not found', () => {
       return request
-        .get("/hello")
+        .get('/hello')
         .expect(404)
-        .then(res => {
+        .then((res) => {
           expect(res.body.msg).to.equal('Page not found');
         });
     });
   });
-  describe("/api/users/:username", () => {
-    it('GET user by username returns status 200 and object of user data', () => {
-      return request
-      .get(`/api/users/${users[0].username}`)
-      .expect(200)
-      .then(res => {
-        expect(res.body.user).to.include.keys(
-          "username",
-          "firstName",
-          "lastName",
-          "email",
-        )
-      })
-    } )
-  })
+
+  describe('/users', () => {
+    describe('/api/users/:username', () => {
+      it('GET user by username returns status 200 and object of user data', () => {
+        return request
+          .get(`/api/users/${users[0].username}`)
+          .expect(200)
+          .then((res) => {
+            expect(res.body.user).to.include.keys(
+              'username',
+              'firstName',
+              'lastName',
+              'email',
+            );
+          });
+      });
+      it('POST user returns status 201 and a new user', () => {
+        const newUser = {
+          username: 'blinky',
+          email: 'blinkyboi@email.com',
+          firstName: 'blinky',
+          lastName: 'the fish',
+        };
+        return request
+          .post('/api/users')
+          .send(newUser)
+          .expect(201)
+          .then(({ body }) => {
+            expect(body).to.include.keys(
+              'username', 'firstName', 'lastName', 'email', 'profilePicture', 'address',
+            );
+            expect(body.username).to.equal(newUser.username);
+          });
+      });
+    });
+  });
 });
